a3a015851ff4cef91389fd702d5f159fdaf67c68
simonojok19 <simonojok19@gmail.com>
2020-03-26 20:02:08 +0300

    added the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './components/create/create.component';
+import { IndexComponent } from './components/index/index.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'index', component: IndexComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
