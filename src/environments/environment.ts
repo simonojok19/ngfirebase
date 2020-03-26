07bb424d4c701a45a364cb2fa852b0ef6fa5798d
simonojok19 <simonojok19@gmail.com>
2020-03-26 19:45:58 +0300

    added the firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,16 @@
 
 export const environment = {
   production: false,
-
+  firebaseConfig: {
+    apiKey: "AIzaSyBA_deQ6tLLTqrgAy7kFW2ltA7zPlpGmS4",
+    authDomain: "ngfirebase-390a0.firebaseapp.com",
+    databaseURL: "https://ngfirebase-390a0.firebaseio.com",
+    projectId: "ngfirebase-390a0",
+    storageBucket: "ngfirebase-390a0.appspot.com",
+    messagingSenderId: "938691235766",
+    appId: "1:938691235766:web:f3832c26f3595e24733b17",
+    measurementId: "G-BWV3NP80P6"
+  };
 };
 
 /*
