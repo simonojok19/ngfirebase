996941d58a346d568c47ffbb7b88357925d9ac16
simonojok19 <simonojok19@gmail.com>
2020-03-27 07:07:31 +0300

    dropping the work

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ShareService } from 'src/app/share.service';
 
 @Component({
   selector: 'app-create',
@@ -7,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateComponent implements OnInit {
   title = 'Share Market';
+  angForm: FormGroup;
+  name: FormControl;
+  price: FormControl;
 
-  constructor() { }
+  constructor(private shareService: ShareService, private fb: FormBuilder) {
+    this.createForm();
+   }
 
   ngOnInit(): void {
+  }
+
+  createForm() {
+    this.angForm = this.fb.group({
+      name: ['', Validators.required],
+      price: ['', Validators.required]
+    });
+  }
+
+  addShare(name, price) {
+    const dataObj = {
+      name: name,
+      price: price
+    };
+    this.shareService.addShare(dataObj);
   }
 
 }
--- src/app/share.service.ts
+++ src/app/share.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShareService {
+  private basePath = '/shares';
+  public items: any;
+  public item: any;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  addShare(data) {
+    const obj = this.db.database.ref(this.basePath);
+    obj.push(data).then((data) => console.log(data)).catch(e => console.log(e));
+    console.log('Success')
+  }
 }
